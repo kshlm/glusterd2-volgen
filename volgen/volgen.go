e20e345ed1565a54882cf5a5fa41f789adc587b5
Anand <anekkunt@redhat.com>
2015-08-02 20:04:50 +0530

    volgen dump implementayion
    Signed-off-by: Anand <anekkunt@redhat.com>

--- volgen/volgen.go
+++ volgen/volgen.go
@@ -1,9 +1,27 @@
 package volgen
 
 import (
+	"fmt"
 	"io"
 )
 
 func (x Xlator) DumpGraph(w io.Writer) {
-	_, _ = w.Write([]byte("Dump function not implemented yet"))
+
+	for _, xl := range x.Children {
+		xl.DumpGraph(w)
+	}
+
+	str := fmt.Sprintf("volume %s\n", x.Name)
+	str += fmt.Sprintf("   type %s", x.Type)
+	flag := true
+	for _, ch_xl := range x.Children {
+		if flag {
+			str += fmt.Sprintf("\n   subvolumes")
+			flag = false
+		}
+		str += fmt.Sprintf(" %s", ch_xl.Name)
+	}
+	str += fmt.Sprintf("\nend-volume\n\n")
+
+	_, _ = w.Write([]byte(str)) //Write into given interface
 }
