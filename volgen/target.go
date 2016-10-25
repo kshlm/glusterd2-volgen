adcab0375634061938ed0f950bac22a7e6c3b544
Kaushal M <kshlmster@gmail.com>
2016-10-25 19:18:17 +0530

    Add a target loader

---
+++ volgen/target.go
@@ -0,0 +1,68 @@
+package volgen
+
+import (
+	"errors"
+	"os"
+	"path/filepath"
+)
+
+type Target struct {
+	*Node
+	Xlators []string
+}
+
+var (
+	ERR_PATH_NOT_TARGET = errors.New("provided path is not a target")
+)
+
+const (
+	targetNodeFile = "info"
+)
+
+func LoadTarget(path string) (*Target, err) {
+	// Ensure the path has targetExt as extension and is a directory
+	if filepath.Ext(path) != targetExt {
+		return nil, ERR_PATH_NOT_TARGET
+	}
+
+	d, e := os.Stat(path)
+	if e != nil {
+		return nil, e
+	}
+	if !d.IsDir() {
+		return nil, ERR_PATH_NOT_TARGET
+	}
+
+	// Load node file
+	nf := filepath.Join(path, targetNodeFile)
+	n, e := NodeFromFile(nf)
+	if e != nil {
+		return nil, e
+	}
+
+	t := &Target{
+		*Node: n,
+	}
+
+	// Load target xlators
+	e := filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if filepath.Ext(path) != xlatorExt {
+			return nil
+		}
+		// Adding the filenames as the Xlator ids
+		// TODO: Add actual Node from xlatorMap
+		t.Xlators = append(t.Xlators, filepath.Base(path))
+
+		return nil
+	})
+	if e != nil {
+		return nil, e
+	}
+
+	// TODO: Need to make sure stuff in Target.Requires are in Target.Xlators
+
+	return t, nil
+}
